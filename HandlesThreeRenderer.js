68e283b3148bd0283d277e5fd9003a93f98bcb19
Florian Steinschorn <f.steinschorn@gmail.com>
2016-07-16 13:58:30 +0200

    position change

--- HandlesThreeRenderer.js
+++ HandlesThreeRenderer.js
@@ -111,8 +111,8 @@
         var buttonDiv = document.createElement('div');
         buttonDiv.id = "buttonDiv";
         buttonDiv.style.position = "relative";
-        buttonDiv.style.left = "50px";
-        buttonDiv.style.top = "-50px";
+        buttonDiv.style.left = x + "px";
+        buttonDiv.style.top = "-" + y + "px";
         buttonDiv.style.z_index = -1;
 
         //create buttons
